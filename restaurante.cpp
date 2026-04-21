75014732f28e20c6cfd0e0b9f43b58a9f9d7d7d7
Dayana Julissa Majano Cruz <00277526@uca.edu.sv>
2026-04-21 12:19:14 -0600

    Add files via upload

---
+++ restaurante.cpp
@@ -0,0 +1,17 @@
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    float p, servicio, total;
+    cout<<"ingresar precio total:";
+    cin>> p;
+
+     servicio = p * 0.10;
+
+     total = p + servicio ;
+
+cout<<"El total a pagar es:"<<total<<endl;
+  return 0;
+
+}
